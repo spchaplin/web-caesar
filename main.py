b1cb909c40703481be3d92ba6336cbe0056c1d1f
Scott Chaplin <scott.chaplin@gmail.com>
2018-09-15 22:23:10 -0500

    form submission with post method successful, text encoding successful

---
+++ main.py
@@ -0,0 +1,57 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """ 
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+     <form method="post">
+    <label>
+        Rotate by:
+        <input name="rot" type="text" value="0">
+    </label>
+
+    <textarea name="text" placeholder="Type message to encode here..."></textarea>
+
+    <input type="submit">
+</form>
+    </body>
+</html>
+
+"""
+
+#url where index function receives requests.  application server sends requests here.
+@app.route("/")
+#index function handles initial GET request from browser.  This is what user see when app first runs.
+def index():
+    return form
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    txt = str(request.form['text'])
+    rotation = int(request.form['rot'])
+    encrypted = rotate_string(txt, rotation)
+    return "<h1>" + encrypted + "</h1>"
+
+app.run()
